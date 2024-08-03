ccc9515f8eba3d7ea6eaeb57dd7c4bc0e3c0ce4e
adan <zizkoteam@gmail.com>
2024-08-03 21:29:58 +0300

    fixed bug

--- models.py
+++ models.py
@@ -3,14 +3,6 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-
 class Artwork(db.Model, SerializerMixin):
     __tablename__ = 'artworks'
     id = db.Column(db.Integer, primary_key=True)
