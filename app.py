ddbb16bc2e5935ff05027518b66ac2ba6e095c84
adan <zizkoteam@gmail.com>
2024-08-20 13:59:26 +0300

    fix deployment

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ app.config['MAIL_DEFAULT_SENDER'] = 'zizkoteam@gmail.com'
 mail = Mail(app)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 sio = Server(cors_allowed_origins=["https://the-met-gallery-frontend-nbi7.vercel.app"])
-app.mount("/", ASGIApp(sio, other_asgi_app=app))
+# app.mount("/", ASGIApp(sio, other_asgi_app=app))
 
 
 ma = Marshmallow(app)
